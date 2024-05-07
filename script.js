9b81232895487fe38b7b2fc82e8224ba56a9e7f7
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-07 15:55:53 +0200

    created const

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const userName=prompt('type a name');
+const userSurname=prompt('type a surname');
+const userFavouriteColor=prompt('type a color');
+const userNumber=prompt('type a number');
+
+console.log(userName + userSurname + userFavouriteColor + userNumber);
+
 
